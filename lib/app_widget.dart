3a229260aed6a7745a67340d78b227f501a97afd
Lucas <lucas.braganca@clubpetro.com>
2021-07-15 21:04:34 -0300

    Shared preferences

--- lib/app_widget.dart
+++ lib/app_widget.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:pay_flow/modules/splash/splash_page.dart';
 import 'package:pay_flow/shared/themes/app_colors.dart';
 
+import 'modules/home/home_page.dart';
 import 'modules/login/login_page.dart';
 
 class AppWidget extends StatelessWidget {
@@ -11,7 +13,12 @@ class AppWidget extends StatelessWidget {
       theme: ThemeData(
         primaryColor: AppColors.primary
       ),
-      home: LoginPage()
+      initialRoute: '/splash',
+      routes: {
+        '/splash': (context) => SplashPage(),
+        '/home': (context) => HomePage(),
+        '/login': (context) => LoginPage()
+      },
     );
   }
 }
