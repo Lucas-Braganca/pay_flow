be55bcfc7d442a0dfb489f7e2e8b076ed421693c
Lucas <lucas.braganca@clubpetro.com>
2021-07-18 20:55:55 -0300

    barcode scanner fix

--- lib/modules/barcode_scanner/barcode_scanner_status.dart
+++ lib/modules/barcode_scanner/barcode_scanner_status.dart
@@ -4,28 +4,30 @@ class BarcodeScannerStatus {
   final bool isCameraAvailable;
   final String error;
   final String barcode;
+final bool stopScanner;
 
-  CameraController? cameraController;
 
   BarcodeScannerStatus({
     this.isCameraAvailable = false,
     this.error = '',
     this.barcode = '',
-    this.cameraController,
+    this.stopScanner = false,
   });
 
-  factory BarcodeScannerStatus.available(CameraController controller) =>
+  factory BarcodeScannerStatus.available() =>
       BarcodeScannerStatus(
         isCameraAvailable: true,
-        cameraController: controller,
+        stopScanner: false,
       );
 
   factory BarcodeScannerStatus.error(String message) => BarcodeScannerStatus(
         error: message,
+        stopScanner: true
       );
 
   factory BarcodeScannerStatus.barcode(String barcode) => BarcodeScannerStatus(
         barcode: barcode,
+        stopScanner: true
       );
 
   bool get showCamera => isCameraAvailable && error.isEmpty;
