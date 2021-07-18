3f22dc0261d2613371cda7a2fe1ec3c89f15628a
Lucas <lucas.braganca@clubpetro.com>
2021-07-17 22:43:04 -0300

    set label buttons component

--- lib/modules/barcode_scanner/barcode_scanner_page.dart
+++ lib/modules/barcode_scanner/barcode_scanner_page.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:pay_flow/shared/themes/app_colors.dart';
+import 'package:pay_flow/shared/themes/app_text_styles.dart';
+import 'package:pay_flow/shared/widgets/divider_vertical/divider_vertical_widget.dart';
+import 'package:pay_flow/shared/widgets/label_button/label_button.dart';
+import 'package:pay_flow/shared/widgets/set_label_buttons/set_label_buttons.dart';
 
 class BarcodeScannerPage extends StatefulWidget {
-  const BarcodeScannerPage({ Key? key }) : super(key: key);
+  const BarcodeScannerPage({Key? key}) : super(key: key);
 
   @override
   _BarcodeScannerPageState createState() => _BarcodeScannerPageState();
@@ -10,8 +15,52 @@ class BarcodeScannerPage extends StatefulWidget {
 class _BarcodeScannerPageState extends State<BarcodeScannerPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      
+    return SafeArea(
+      top: true,
+      bottom: true,
+      left: true,
+      right: true,
+      child: RotatedBox(
+        quarterTurns: 1,
+        child: Scaffold(
+            appBar: AppBar(
+              backgroundColor: Colors.black,
+              title: Text(
+                'Escaneie o código de barras do boleto',
+                style: TextStyles.buttonBackground,
+              ),
+              centerTitle: true,
+              leading: BackButton(
+                color: AppColors.background,
+              ),
+            ),
+            body: Column(
+              children: [
+                Expanded(
+                  child: Container(
+                    color: Colors.black.withOpacity(0.6),
+                  ),
+                ),
+                Expanded(
+                  flex: 2,
+                  child: Container(
+                    color: Colors.transparent,
+                  ),
+                ),
+                Expanded(
+                  child: Container(
+                    color: Colors.black.withOpacity(0.6),
+                  ),
+                ),
+              ],
+            ),
+            bottomNavigationBar: SetLabelButtons(
+              primaryLabel: 'Inserir código do boleto',
+              primaryOnPressed: () {},
+              secondaryLabel: 'Adicionar da galeria',
+              secondaryOnPressed: () {},
+            )),
+      ),
     );
   }
 }
