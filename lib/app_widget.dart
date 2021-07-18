7d09a888c6ad058a482b6a28ebabfe77b9d1da35
Lucas <lucas.braganca@clubpetro.com>
2021-07-18 12:00:51 -0300

    App orientation

--- lib/app_widget.dart
+++ lib/app_widget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:pay_flow/modules/barcode_scanner/barcode_scanner_page.dart';
 import 'package:pay_flow/modules/splash/splash_page.dart';
 import 'package:pay_flow/shared/themes/app_colors.dart';
@@ -7,6 +8,15 @@ import 'modules/home/home_page.dart';
 import 'modules/login/login_page.dart';
 
 class AppWidget extends StatelessWidget {
+
+  AppWidget(){
+    SystemChrome.setPreferredOrientations(
+      [
+        DeviceOrientation.portraitDown,
+        DeviceOrientation.portraitUp
+      ]
+    );
+  }
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
